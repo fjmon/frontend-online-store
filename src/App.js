e31a7ab3c7c10356d786930649a281c15448ecd7
iahgo <iahgo@icloud.com>
2022-07-08 15:50:28 -0300

    foi conferido req 1 e 2, e correção do 3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
-import './App.css';
+import CampoPesquisa from './CampoPesquisa';
 import Botao from './Botao';
 import ShoppingCart from './ShoppingCart';
 
@@ -10,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Botao />
       <Switch>
-        {/* <Route path="/" component={ CampoPesquisa } /> */}
+        <Route exact path="/" component={ CampoPesquisa } />
         <Route exact path="/ShoppingCart">
           <ShoppingCart />
         </Route>
